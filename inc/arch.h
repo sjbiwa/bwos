19fff2569b9aadab14f4d193e57353890473dadc
biwa <sjbiwa@dream.com>
2014-11-24 13:28:32 +0900

    ビルド確認 テストコード追加 (for cygwin)

---
+++ inc/arch.h
@@ -0,0 +1,16 @@
+/*
+ * arch.h
+ *
+ *  Created on: 2014/11/24
+ *      Author: biwa
+ */
+
+#ifndef ARCH_H_
+#define ARCH_H_
+
+#if defined(__arm__)
+#include "arm.h"
+#endif
+
+
+#endif /* ARCH_H_ */
