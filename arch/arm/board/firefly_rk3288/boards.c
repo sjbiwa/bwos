029aeb67e757ced98d8dad5630c0c51941ae0e90
biwa <sjbiwa@dream.com>
2015-05-08 01:13:11 +0900

    SPIドライバ修正中

--- arch/arm/board/firefly_rk3288/boards.c
+++ arch/arm/board/firefly_rk3288/boards.c
@@ -9,6 +9,7 @@
 #include "driver/clock.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
+#include "driver/spi.h"
 #include "irqdefs.h"
 #include "ioregs.h"
 #include "cruregs.h"
@@ -19,6 +20,10 @@
 #define	CLOCK_UART2			2
 #define	CLOCK_UART3			3
 #define	CLOCK_UART4			4
+#define	CLOCK_SPI0			5
+#define	CLOCK_SPI1			6
+#define	CLOCK_SPI2			7
+
 
 static const uint32_t	cru = CRU_REG_BASE;
 static const uint32_t	grf = GRF_REG_BASE;
@@ -26,11 +31,14 @@ static const uint32_t	lvds = LVDS_REG_BASE;
 static const uint32_t	pmu = PMU_REG_BASE;
 
 static ClockRegisterParam clock_params[] = {
-	[CLOCK_UART0] = { 24000000 },
-	[CLOCK_UART1] = { 24000000 },
-	[CLOCK_UART2] = { 24000000 },
-	[CLOCK_UART3] = { 24000000 },
-	[CLOCK_UART4] = { 24000000 },
+	[CLOCK_UART0] 	= { 24000000 },
+	[CLOCK_UART1] 	= { 24000000 },
+	[CLOCK_UART2] 	= { 24000000 },
+	[CLOCK_UART3] 	= { 24000000 },
+	[CLOCK_UART4] 	= { 24000000 },
+	[CLOCK_SPI0]	= { 96000000 },
+	[CLOCK_SPI1]	= {        0 },	/* not used */
+	[CLOCK_SPI2]	= { 96000000 },
 };
 
 static UartDeviceInfo uart_info[] = {
@@ -53,8 +61,22 @@ static GpioDeviceInfo gpio_info[] = {
 		{ GPIO8_REG_BASE },								/* GPIO8 */
 };
 
+static SpiDeviceInfo spi_info[] = {
+		{ SPI0_REG_BASE,  IRQ_SPI0,  CLOCK_SPI0},	/* SPI0  */
+		{ SPI1_REG_BASE,  IRQ_SPI1,  CLOCK_SPI1},	/* SPI1 (not used)  */
+		{ SPI2_REG_BASE,  IRQ_SPI2,  CLOCK_SPI2},	/* SPI2  */
+};
+
 void init_task_board_depend(void)
 {
+	/****************************************/
+	/* firefly3288: 						*/
+	/*  arm pll    = 600000000HZ			*/
+	/*  periph pll = 297000000HZ			*/
+	/*  ddr pll    = 200000000HZ			*/
+	/*  codec pll  = 384000000HZ			*/
+	/****************************************/
+
 	/************************/
 	/* clock configure		*/
 	/************************/
@@ -63,6 +85,9 @@ void init_task_board_depend(void)
 	iowrite32(cru+CRU_CLKSEL16_CON, 0xffff0200); /* UART_GPS / 24MHz */
 	iowrite32(cru+CRU_CLKSEL3_CON, 0xffff0200); /* UART_EXP / 24MHz */
 	iowrite32(cru+CRU_CLKSEL27_CON, 0xffff0400); /* LCDC0 clock */
+	iowrite32(cru+CRU_CLKSEL25_CON, 0xffff0707); /* SPI0/SPI1 clock(codec pll / 8 = 48MHz) */
+	iowrite32(cru+CRU_CLKSEL39_CON, 0x00ff0007); /* SPI02 clock(codec pll / 8 = 48MHz) */
+
 	clock_register(clock_params, arrayof(clock_params));
 
 	/************************/
@@ -100,8 +125,14 @@ void init_task_board_depend(void)
 	/* for LED */
 	iowrite32(grf+GRF_GPIO8A_IOMUX, 0x003c0000); /* GPIO8A[2](POWER_LED)/[1](WORK_LED) */
 
+	/* for SPI0/2 (SPI1 not used) */
+	iowrite32(grf+GRF_GPIO5B_IOMUX, 0xff005500); /* SPI0:RXD/TXD/CSN0/CLK */
+	iowrite32(grf+GRF_GPIO5C_IOMUX, 0x00030001); /* SPI0:CSN1 */
+	iowrite32(grf+GRF_GPIO8A_IOMUX, 0xf0C05040); /* SPI2:CSN0/CLK/CSN1 */
+	iowrite32(grf+GRF_GPIO8B_IOMUX, 0x000f0005); /* SPI2:TXD/RXD */
 
 	/* register device */
 	uart_register(&uart_info, arrayof(uart_info));
 	gpio_register(&gpio_info, arrayof(gpio_info));
+	spi_register(&spi_info, arrayof(spi_info));
 }
--- include/driver/spi.h
+++ include/driver/spi.h
@@ -9,7 +9,12 @@
 #define SPI_H_
 
 
+typedef	struct {
+	uint32_t		io_addr;				/* ベースアドレス */
+	uint32_t		irq;					/* IRQ番号 */
+	uint32_t		clock_src;				/* クロック識別子 */
+} SpiDeviceInfo;
 
-
+extern void spi_register(SpiDeviceInfo* info, uint32_t info_num);
 
 #endif /* SPI_H_ */
